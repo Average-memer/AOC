fe283e360b1e289e3c547d8746ab53283a8e1176
Average-memer <92406852+Average-memer@users.noreply.github.com>
2023-12-03 04:56:11 +0100

    Add files via upload

---
+++ template.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <sstream>
+
+int main(int argc, char const *argv[])
+{
+	std::cout << "Merry Crisis"
+	return 0;
+}

f0b0f6c99301665c2756b8a5f304aa1c6c8fb243
Average-memer <92406852+Average-memer@users.noreply.github.com>
2023-12-05 20:26:26 +0100

    Update template.cpp
    Add more usings and some helper functions I use often

--- template.cpp
+++ template.cpp
@@ -4,6 +4,40 @@
 #include <algorithm>
 #include <vector>
 #include <sstream>
+#include <cstdio>
+#include <cassert>
+#include <iterator>
+#include <array>
+#include <math.h>
+#include <numeric>
+
+using std::vector;
+using std::string;
+using std::size_t;
+using std::cout, std::endl;
+
+
+template <typename T>
+void printVector(const vector<T>& vec, bool newline, char seperator){
+	for (auto item : vec)
+	{
+		std::cout << item << seperator;
+	}
+	if (newline)
+	{
+		std::cout << std::endl; 
+	}	
+}
+
+vector<string> readLines(){
+	std::ifstream file("input");
+	string line;
+	vector<string> lines;
+    while (std::getline(file, line)) {
+        lines.push_back(line);
+    }
+    return lines;
+}
 
 int main(int argc, char const *argv[])
 {
